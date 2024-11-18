c4152ccf224d64122cd437991d1e3cd1ade8c1c7
HeyItsDuke <40841908+HeyItsDuke@users.noreply.github.com>
2024-11-18 00:00:09 -0500

    Added copyright notice
    Added copyright notice as comment in the code as its author

--- hf2_generate_lua_db.py
+++ hf2_generate_lua_db.py
@@ -210,3 +210,11 @@ LOG_FILE.close()
 
 # Final message to the console
 print(f"Process complete. Log saved to '{LOG_FILE_PATH}'.")
+
+# Copyright (c) 2024 HeyItsDuke
+
+# This project is licensed under the Mozilla Public License, Version 2.0 (MPL-2.0). You may obtain a copy of the License at:
+
+# https://opensource.org/licenses/MPL-2.0
+
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations.
